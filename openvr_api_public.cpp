c354355a28e5dc1641892c0670297e70e210af22
Christoph Haag <haagch@frickel.club>
2017-10-18 11:56:15 +0200

    foo 2

--- openvr_api_public.cpp
+++ openvr_api_public.cpp
@@ -765,14 +765,17 @@ public:
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[0][0] = m[0][0];
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[0][1] = m[0][1];
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[0][2] = m[0][2];
+            pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[0][3] = m[0][3];
 
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[1][0] = m[1][0];
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[1][1] = m[1][1];
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[1][2] = m[1][2];
+            pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[1][3] = m[1][3];
 
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[2][0] = m[2][0];
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[2][1] = m[2][1];
             pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[2][2] = m[2][2];
+            pRenderPoseArray[0].mDeviceToAbsoluteTracking.m[2][3] = m[2][3];
 
 
             pRenderPoseArray[0].eTrackingResult = ETrackingResult::TrackingResult_Running_OK;
